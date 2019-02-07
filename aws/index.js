6a09918203b5663361f5f3560a1f24ad34635827
Киберговночерпий <autechregescom@gmail.com>
2019-02-07 08:38:01 +0300

    refactor backend responses

--- aws/index.js
+++ aws/index.js
@@ -1,13 +1,11 @@
 const fetch = require('node-fetch')
 const jsdom = require('jsdom')
-const { JSDOM } = jsdom;
+const {JSDOM} = jsdom;
 
 
 const getContent = async (url) => {
     return new Promise((resolve, reject) => {
         const request = require('https').get(url, (response) => {
-            console.log(response.statusCode)
-
             const body = [];
 
             response.on('data', (chunk) => body.push(chunk))
@@ -31,13 +29,18 @@ class LastFMHTMLClient {
                 const dom = new JSDOM(html)
                 const nodes = Array.from(dom.window.document.querySelectorAll('.big-artist-list-title'))
 
-                return nodes.map(node => {
-                    return node.textContent
-                })
+                return {
+                    error: false,
+                    response: nodes.map(node => {
+                        return node.textContent
+                    })
+                }
             })
             .catch(error => {
-                console.log('html fetch error', error)
-                return []
+                return {
+                    error: true,
+                    response: error
+                }
             })
     }
 }
@@ -68,58 +71,77 @@ class LastFMClient {
             .then(response => JSON.parse(response))
             .catch(response => response)
 
-        const isError = response.hasOwnProperty('error')
-
-        if (isError) {
-            return []
+        if (response.hasOwnProperty('error')) {
+            return {
+                error: true,
+                response: response
+            }
         }
 
-        return response.similartracks.track.map(({ name, artist }) => {
-            return {
-                artist: artist.name,
-                track: name
-            }
-        })
+        return {
+            error: false,
+            response: response.similartracks.track.map(({name, artist}) => {
+                return {
+                    artist: artist.name,
+                    track: name
+                }
+            })
+        }
     }
 
     async getSimilarArtists(artist) {
         const response = await getContent(`${this.apiRoot}?method=artist.getsimilar&autocorrect=1&artist=${artist}&api_key=${this.token}&format=json`)
             .then(response => JSON.parse(response))
 
-        const isError = response.hasOwnProperty('error')
-
-        if (isError) {
-            return []
+        if (response.hasOwnProperty('error')) {
+            return {
+                error: true,
+                response: response
+            }
         }
 
-        return response.similarartists.artist.map(({ name, image }) => name)
+        return {
+            error: false,
+            response: response.similarartists.artist.map(({name}) => name)
+        }
     }
 
     async getRandomTopTracks(artists, chooseFrom) {
         return Promise.all(
             artists.map(async artist => {
-                const response = await getContent(`${this.apiRoot}?method=artist.gettoptracks&autocorrect=1&artist=${encodeURIComponent(artist)}&api_key=${this.token}&format=json`)
-                    .catch(response => [])
-                    .then(response => JSON.parse(response))
+                const topTracks = await getContent(`${this.apiRoot}?method=artist.gettoptracks&autocorrect=1&artist=${encodeURIComponent(artist)}&api_key=${this.token}&format=json`)
+                    .catch(response => {
+                        return {
+                            error: true,
+                            response: JSON.parse(response)
+                        }
+                    })
+                    .then(response => {
+                            return {
+                                error: false,
+                                response: JSON.parse(response).toptracks.track
+                            }
+                        }
+                    )
 
-                const top = response.toptracks.track.slice(0, chooseFrom).map(({ name, artist }) => {
+                if (topTracks.error) {
+                    return null
+                }
+
+                const top = topTracks.response.slice(0, chooseFrom).map(({name, artist}) => {
                     return {
                         artist: artist.name,
                         track: name
                     }
                 })
 
-                if (!top) {
+                if (!top.length) {
                     return null
                 }
 
                 return shuffle(top)[0]
             })
         )
-    }
-
-    async getArtistInfo(artist) {
-        return await getContent(`${this.apiRoot}?method=`)
     }
 }
 
@@ -135,18 +157,18 @@ exports.handler = async (event, context, callback) => {
     if (event.httpMethod !== 'GET') {
         return callback(null, {
             statusCode: 405,
-            body: JSON.stringify({ 'error': 'not allowed' }),
+            body: JSON.stringify({'error': 'not allowed'}),
             headers: {
                 'Content-Type': 'application/json',
             }
         })
     }
 
-    const client = new LastFMClient('74c6ff6a9c1edd5572557e2d63eb1bc3')
-
-    let { artist, track, unwanted } = event.queryStringParameters
-    artist = encodeURIComponent(artist)
-    track = encodeURIComponent(track)
+    let {
+        artist = encodeURIComponent(artist),
+        track = encodeURIComponent(track),
+        unwanted
+    } = event.queryStringParameters
 
     if (unwanted) {
         try {
@@ -156,27 +178,33 @@ exports.handler = async (event, context, callback) => {
         }
     }
 
-    const similarTracks = await client.getSimilarTracks(artist, track)
-    let response = similarTracks
+    const apiClient = new LastFMClient('74c6ff6a9c1edd5572557e2d63eb1bc3')
+    const similarTracks = await apiClient.getSimilarTracks(artist, track)
 
-    if (!response.length) {
-        let similarArtists = await client.getSimilarArtists(artist)
+    let statusCode = 200
+    let response = []
 
-        // TODO: do not fetch anything if artist does not have tags
+    if (!similarTracks.error && similarTracks.response.length) {
+        response = similarTracks.response
+    }
+
+    if (similarTracks.error || !similarTracks.response.length) {
+        let similarArtists = await apiClient.getSimilarArtists(artist)
 
         /**
-         * sometimes lastfm api does not return similar artists, 
+         * sometimes lastfm api does not return similar artists,
          * even though they are displayed on the web page of artist
          * let's grab html
          */
-        if (!similarArtists.length) {
+        if (similarArtists.error || !similarArtists.length) {
             const htmlClient = new LastFMHTMLClient()
             similarArtists = await htmlClient.getSimilarArtists(artist)
         }
 
         const randomArtists = shuffle(similarArtists.slice(0, 30))
-        const tracks = await client.getRandomTopTracks(randomArtists, 4)
+        const tracks = await apiClient.getRandomTopTracks(randomArtists, 4)
 
+        // get rid of null
         response = tracks.filter(track => track)
     }
 
@@ -186,7 +214,7 @@ exports.handler = async (event, context, callback) => {
     }
 
     callback(null, {
-        statusCode: 200,
+        statusCode: statusCode,
         body: JSON.stringify(response),
         headers: {
             'Content-Type': 'application/json',
