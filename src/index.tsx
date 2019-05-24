9e6f1771f554f90846b7d5a8a6acc367b726afae
ixnv <autechregescom@gmail.com>
2019-05-24 03:10:55 +0300

    switch to styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,19 @@
-function foo(a: string, b: string, ...c: string[]): string[] {
-    return c
-}
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+
+import store from './redux/store'
+import {App} from './app'
+
+const wrapperClass = 'vk-lastfm-recommendations'
+
+const wrapper = document.createElement('div')
+wrapper.setAttribute('class', wrapperClass)
+document.body.appendChild(wrapper)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.querySelector(`.${wrapperClass}`)
+)
