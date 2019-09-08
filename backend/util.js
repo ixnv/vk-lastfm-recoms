8fa93ec79dd0353eb7bda9cdeaa2cdeabc061ac9
Ramil Iksanov <autechregescom@gmail.com>
2019-09-08 15:43:03 +0300

    fkn lol

--- backend/util.js
+++ backend/util.js
@@ -1,7 +1,7 @@
 export const shuffle = arr => [...arr].sort(() => 0.5 - Math.random())
 
 export const objectToQueryString = obj => {
-    Object.keys(obj).reduce((acc, k) => {
+    return Object.keys(obj).reduce((acc, k) => {
         acc.push(`${k}=${obj[k]}`)
         return acc
     }, []).join('&')
